e325cd68eaa33060140eb7f3e4c19a2f40fdafe4
Matheu DeWitt <msdewitt@bsu.edu>
2018-03-26 16:47:41 -0400

    Add config files.

--- src/main/java/com/evilscheme/registryservice/RegistryServiceApplication.java
+++ src/main/java/com/evilscheme/registryservice/RegistryServiceApplication.java
@@ -2,8 +2,12 @@ package com.evilscheme.registryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableHystrixDashboard
 public class RegistryServiceApplication {
 
 	public static void main(String[] args) {
